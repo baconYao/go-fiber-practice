4a061d1eed3e5a1778737494ece1997959b536d1
peiyao <peiyaochang@qnap.com>
2021-04-18 09:59:11 +0800

    Add migration for db's User model

--- database/connect.go
+++ database/connect.go
@@ -1,14 +1,17 @@
 package database
 
 import (
+	"github.com/baconYao/go-fiber-practice/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() {
-	_, err := gorm.Open(mysql.Open("root:rootroot@/go_admin"), &gorm.Config{})
+	database, err := gorm.Open(mysql.Open("root:rootroot@/go_admin"), &gorm.Config{})
     
     if err != nil {
         panic("Could not connect to the database")
     }
+
+	database.AutoMigrate(&models.User{})
 }
